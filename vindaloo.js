6b655abc54d57080d45c1330783bfcb77f629638
Ray <raitis.cerkasovs@gmail.com>
2020-10-22 21:03:46 +0100

    move to node ws library

--- vindaloo.js
+++ vindaloo.js
@@ -6,11 +6,7 @@ var app = express();
 app.set("view engine", "ejs");
 app.use("/assets", express.static(__dirname + "/public"));
 
-app.get("/", function(req, res) {
-    res.render("index", { puppet: conf.login.user, puddle: conf.login.password });
-});
-
-var mainApiController = require('./controllers/mainApiController');
-//mainApiController.run(app);
+var mainController = require('./controllers/mainController');
+mainController.run(app);
 
 app.listen(conf.app.port);
