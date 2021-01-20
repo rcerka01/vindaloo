3561a441f224e61b44befd91a3480e0a9ecf53c5
Ray <raitis.cerkasovs@gmail.com>
2021-01-20 19:36:51 +0000

    add start stop day

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,6 +1,10 @@
 const conf = require("../config/config");
 const send = require("./wsSendRequests");    
+const closeTradesController = require("./closeTradesController");    
 const WebSocket = require('ws');
+const schedule = require('node-schedule');
+
+var TIME_DEPENDANT_RUN_FLAG = false;
  
 function connect() {
     var url = conf.ws.url;
@@ -30,9 +34,38 @@ function isExistingTrade(trades, symbol, cmd) {
     return trades.some(item =>  item.cmd === cmd && item.symbol === symbol )
 }
 
-module.exports = { run: function (app) {
+function formatScheduleTime(time) {
+    const formatedTime = '0 ' + time.minute + ' ' + time.hour + ' * * 1-5';
+    return formatedTime;
+}
 
-    app.post("/:account/:sl/:offset/:tp/:action/:symbol/:volume", function(req, res) {
+
+function openDay() {
+    schedule.scheduleJob(formatScheduleTime(conf.timedependant.openTime), function(){
+        console.log('OPEN DAY');
+        TIME_DEPENDANT_RUN_FLAG = true;
+    });
+
+}
+
+function closeDay() {
+    schedule.scheduleJob(formatScheduleTime(conf.timedependant.closeTime), function(){
+        console.log('CLOSE DAY');
+        TIME_DEPENDANT_RUN_FLAG = false;
+
+        for (i in conf.timedependant.closeAccounts) {
+            for (ii in conf.timedependant.closeSymbols) {
+                closeTradesController.close(conf.timedependant.closeSymbols[ii], conf.timedependant.closeAccounts[i]);
+            }
+        }        
+    });
+}
+
+module.exports = { run: function (app) {
+    openDay();
+    closeDay();
+
+    app.post("/:account/:sl/:offset/:tp/:action/:symbol/:volume/:timedependant?", function(req, res) {
 
         var account = Number(req.params.account);
         var sl = Number(req.params.sl);
@@ -41,69 +74,82 @@ module.exports = { run: function (app) {
         var action = req.params.action;
         var symbol = req.params.symbol;
         var volume = Number(req.params.volume);
+        var timeDependant = req.params.timedependant;
 
-        const wSocket = connect();
-
-        wSocket.onopen = function() {
-            console.log('Connected');
-            ws = send.login(wSocket, account);
-        };
-
-        wSocket.onmessage = function(evt) {
-            try {
-                var response = JSON.parse(evt.data);
-                if(response.status == true) {
-                    if(response.streamSessionId != undefined) {
-                        console.log("Login successful");
-
-                        send.getPreviousTrades(wSocket);
-
-                    } else if (response.returnData.ask != undefined) {
-                        /* return getPrice */
-
-                        if (action == "sell") { var price = response.returnData.bid; } else { var price = response.returnData.ask; }
-                        send.startTrade(action, symbol, price, volume, wSocket, sl, tp, offset)
-                   
-                    } else if (response.returnData.order != undefined) {
-                        /* return startTrade. Jusr order ID */
-
-                        var order = response.returnData.order;
-                        console.log("Order compleated: " + order); // finish
-
-                        // change condition to for also 0 trades open
-                    } else if (response.returnData.length > 0) {
-                        /* return getPreviousTrades */
-
-                        var cmd = actionStringToInt(action);
-
-                        // only if trade is NOT in same direction (buy / sell)
-                        if (!isExistingTrade(response.returnData, symbol, cmd)) {
-                            send.getPrice(symbol, wSocket) // ignites new order
-                            closeTrades(response.returnData, symbol, wSocket, action);
-                        }
-
-                    } else if (response.returnData.length == 0) {
-                        /* return getPreviousTrades, none exist. Start first one */
-
-                        send.getPrice(symbol, wSocket)
-
-                    } else {
-                        console.log("Disconecting, no action taken.");
-                        //disconnect(wSocket);
-                    }
-                } else {
-                    console.log('Error: ' + response.errorDescr);
-                }
-            } catch (Exception) {
-                console.log('Fatal error while receiving data! :(');
-            }
+        // set lock for time dependant
+        if (timeDependant == 'true' && !TIME_DEPENDANT_RUN_FLAG) {
+            var run = false;
+        } else {
+            var run = true;
         }
 
-        wSocket.onclose = function() {
-            console.log('Connection closed');
-        };
+        // lock time dependant
+        if (run) {
+            const wSocket = connect();
+
+            wSocket.onopen = function() {
+                console.log('Connected');
+                ws = send.login(wSocket, account);
+            };
+
+            wSocket.onmessage = function(evt) {
+                try {
+                    var response = JSON.parse(evt.data);
+                    if(response.status == true) {
+                        if(response.streamSessionId != undefined) {
+                            console.log("Login successful");
+
+                            send.getPreviousTrades(wSocket);
+
+                        } else if (response.returnData.ask != undefined) {
+                            /* return getPrice */
+
+                            if (action == "sell") { var price = response.returnData.bid; } else { var price = response.returnData.ask; }
+                            send.startTrade(action, symbol, price, volume, wSocket, sl, tp, offset)
+                    
+                        } else if (response.returnData.order != undefined) {
+                            /* return startTrade. Jusr order ID */
+
+                            var order = response.returnData.order;
+                            console.log("Order compleated: " + order); // finish
+
+                            // change condition to for also 0 trades open
+                        } else if (response.returnData.length > 0) {
+                            /* return getPreviousTrades */
+
+                            var cmd = actionStringToInt(action);
+
+                            // only if trade is NOT in same direction (buy / sell)
+                            if (!isExistingTrade(response.returnData, symbol, cmd)) {
+                                send.getPrice(symbol, wSocket) // ignites new order
+                                closeTrades(response.returnData, symbol, wSocket, action);
+                            }
+
+                        } else if (response.returnData.length == 0) {
+                            /* return getPreviousTrades, none exist. Start first one */
+
+                            send.getPrice(symbol, wSocket)
+
+                        } else {
+                            console.log("Disconecting, no action taken.");
+                            //disconnect(wSocket);
+                        }
+                    } else {
+                        console.log('Error: ' + response.errorDescr);
+                    }
+                } catch (Exception) {
+                    console.log('Fatal error while receiving data! :(');
+                }
+            }
+
+            wSocket.onclose = function() {
+                console.log('Connection closed');
+            };
+
+        } // close time deendant lock
 
         res.render("index");
         });
+    
     }
 }
