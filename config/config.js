e2cdf65eac86ad100fa56db655dbc5033ed79e70
Ray <raitis.cerkasovs@gmail.com>
2021-03-18 08:51:52 +0000

    c

--- config/config.js
+++ config/config.js
@@ -11,7 +11,7 @@ module.exports = {
     wsLive: {
         url: "wss://ws.xtb.com/real"
     },
-    lockedAccounts: [101,102,104],
+    lockedAccounts: [101,102,103,104],
     multiples: [
         {   // 35
             id: 1001,
