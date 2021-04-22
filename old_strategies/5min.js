c2f770212c96237297bff116d2fafffae17c492b
Ray <raitis.cerkasovs@gmail.com>
2021-04-22 15:28:09 +0100

    leave OIL.WTI for strategy1 only

--- old_strategies/5min.js
+++ old_strategies/5min.js
@@ -1,4 +1,22 @@
-module.exports = {
+module.exports = 
+
+// OIL.WTI
+
+// EU50
+// FRA40
+// US500
+// DE30
+// UK100
+
+// NZDUSD
+// AUDUSD
+// EURUSD
+// USDCAD
+// GBPUSD
+// USDJPY
+// USDCHF
+// EURUSD
+{
     id: 0001,
     name: "5min MA5/20 MACD",
     parameterCount: 4,
@@ -6,6 +24,56 @@ module.exports = {
         {
             symbol: "OIL.WTI",
             account: 777
+        },
+
+        {
+            symbol: "UK100",
+            account: 778
+        }, 
+        {
+            symbol: "DE30",
+            account: 779
+        },
+        {
+            symbol: "US500",
+            account: 780
+        }, 
+        {
+            symbol: "FRA40",
+            account: 781
+        },
+        {
+            symbol: "EU50",
+            account: 782
+        },  
+        
+        {
+            symbol: "EURUSD",
+            account: 791
+        }, 
+        {
+            symbol: "USDCHF",
+            account: 792
+        },
+        {
+            symbol: "USDJPY",
+            account: 793
+        }, 
+        {
+            symbol: "GBPUSD",
+            account: 794
+        },
+        {
+            symbol: "USDCAD",
+            account: 795
+        },
+        {
+            symbol: "AUDUSD",
+            account: 797
+        },
+        {
+            symbol: "NZDUSD",
+            account: 798
         },
     ],
     // and
---
+++ old_strategies/multiplesFor7xx.js
@@ -0,0 +1,367 @@
+module.exports = {
+    accounts: [
+// LIVE //////////////////////////////////////////////////////////////////////
+
+// {   // 35
+//     id: 1001,
+//     accounts: [
+//             {
+//             id: 101,
+//             sl: 25,
+//             tp: 0,
+//             offset: 0,
+//             symbol: 'GBPUSD',
+//             volume: 0.1
+//         }
+//     ]
+// },
+// {   // 40
+//     id: 1002,
+//     accounts: [
+//             {
+//             id: 102,
+//             sl: 25,
+//             tp: 0,
+//             offset: 0,
+//             symbol: 'GBPUSD',
+//             volume: 0.1
+//         }
+//     ]
+// },        
+// {   // 45
+//     id: 1003,
+//     accounts: [
+//             {
+//             id: 103,
+//             sl: 25,
+//             tp: 0,
+//             offset: 0,
+//             symbol: 'GBPUSD',
+//             volume: 0.1
+//         }
+//     ]
+// },        
+// {   // 50 0.0009
+//     id: 1004,
+//     accounts: [
+//             {
+//             id: 104,
+//             sl: 25,        {   
+
+//             tp: 0,
+//             offset: 0,
+//             symbol: 'GBPUSD',
+//             volume: 0.1
+//         }
+//     ]
+// },
+
+// TEST
+        {   
+            id: 9999,
+            accounts: []
+        },
+// STRATEGY 3 //////////////////////////////////////////////////////////////
+        { 
+            id: 577,
+            accounts: [
+                {
+                    id: 18,
+                    sl: 3000,
+                    tp: 0,
+                    offset: 0,
+                    symbol: 'OIL.WTI',
+                    volume: 0.1
+                },
+                {
+                    id: 51,
+                    sl: 3000,
+                    tp: 0,
+                    offset: 0,
+                    symbol: 'OIL.WTI',
+                    volume: 0.1
+                }
+            ]
+        },
+// STRATEGY 1 ////////////////////////////////////////////////////////////////////////        
+        {
+            id: 777,
+            accounts: [
+                {
+                    id: 19,
+                    sl: 3000,
+                    tp: 2000,
+                    offset: 0,
+                    symbol: 'OIL.WTI',
+                    volume: 0.1
+                },
+                {
+                    id: 50,
+                    sl: 3000,
+                    tp: 2000,
+                    offset: 0,
+                    symbol: 'OIL.WTI',
+                    volume: 0.1
+                }
+            ]
+        },
+        {   
+            id: 778,
+            accounts: [
+                {
+                    id: 19,
+                    sl: 300000,
+                    tp: 0,
+                    offset: 0,
+                    symbol: 'UK100',
+                    volume: 0.1
+                },
+                {
+                    id: 50,
+                    sl: 300000,
+                    tp: 0,
+                    offset: 0,
+                    symbol: 'UK100',
+                    volume: 0.1
+                }
+            ]
+        },{   
+            id: 779,
+            accounts: [
+                {
+                    id: 19,
+                    sl: 150000,
+                    tp: 0,
+                    offset: 0,
+                    symbol: 'DE30',
+                    volume: 0.1
+                },
+                {
+                    id: 50,
+                    sl: 150000,
+                    tp: 0,
+                    offset: 0,
+                    symbol: 'DE30',
+                    volume: 0.1
+                }
+            ]
+        },{   
+            id: 780,
+            accounts: [
+                {
+                    id: 19,
+                    sl: 80000,
+                    tp: 0,
+                    offset: 0,
+                    symbol: 'US500',
+                    volume: 0.1
+                },
+                {
+                    id: 50,
+                    sl: 80000,
+                    tp: 0,
+                    offset: 0,
+                    symbol: 'US500',
+                    volume: 0.1
+                }
+            ]
+        },{   
+            id: 781,
+            accounts: [
+                {
+                    id: 19,
+                    sl: 300000,
+                    tp: 0,
+                    offset: 0,
+                    symbol: 'FRA40',
+                    volume: 0.1
+                },
+                {
+                    id: 50,
+                    sl: 300000,
+                    tp: 0,
+                    offset: 0,
+                    symbol: 'FRA40',
+                    volume: 0.1
+                }
+            ]
+        },{   
+            id: 782,
+            accounts: [
+                {
+                    id: 19,
+                    sl: 300000,
+                    tp: 0,
+                    offset: 0,
+                    symbol: 'EU50',
+                    volume: 0.1
+                },
+                {
+                    id: 50,
+                    sl: 300000,
+                    tp: 0,
+                    offset: 0,
+                    symbol: 'EU50',
+                    volume: 0.1
+                }
+            ]
+        },{   
+            id: 791,
+            accounts: [
+                {
+                    id: 19,
+                    sl: 25,
+                    tp: 0,
+                    offset: 0,
+                    symbol: 'EURUSD',
+                    volume: 0.1
+                },
+                {
+                    id: 50,
+                    sl: 25,
+                    tp: 0,
+                    offset: 0,
+                    symbol: 'EURUSD',
+                    volume: 0.1
+                }
+            ]
+        },{   
+            id: 792,
+            accounts: [
+                {
+                    id: 19,
+                    sl: 25,
+                    tp: 0,
+                    offset: 0,
+                    symbol: 'USDCHF',
+                    volume: 0.1
+                },
+                {
+                    id: 50,
+                    sl: 25,
+                    tp: 0,
+                    offset: 0,
+                    symbol: 'USDCHF',
+                    volume: 0.1
+                }
+            ]
+        },{   
+            id: 793,
+            accounts: [
+                {
+                    id: 19,
+                    sl: 0,
+                    tp: 0,
+                    offset: 0,
+                    symbol: 'USDJPY',
+                    volume: 0.1
+                },
+                {
+                    id: 50,
+                    sl: 0,
+                    tp: 0,
+                    offset: 0,
+                    symbol: 'USDJPY',
+                    volume: 0.1
+                }
+            ]
+        },{   
+            id: 794,
+            accounts: [
+                {
+                    id: 19,
+                    sl: 25,
+                    tp: 0,
+                    offset: 0,
+                    symbol: 'GBPUSD',
+                    volume: 0.1
+                },
+                {
+                    id: 50,
+                    sl: 25,
+                    tp: 0,
+                    offset: 0,
+                    symbol: 'GBPUSD',
+                    volume: 0.1
+                }
+            ]
+        },{   
+            id: 795,
+            accounts: [
+                {
+                    id: 19,
+                    sl: 25,
+                    tp: 0,
+                    offset: 0,
+                    symbol: 'USDCAD',
+                    volume: 0.1
+                },
+                {
+                    id: 50,
+                    sl: 25,
+                    tp: 0,
+                    offset: 0,
+                    symbol: 'USDCAD',
+                    volume: 0.1
+                }
+            ]
+        },{   
+            id: 797,
+            accounts: [
+                {
+                    id: 19,
+                    sl: 25,
+                    tp: 0,
+                    offset: 0,
+                    symbol: 'AUDUSD',
+                    volume: 0.1
+                },
+                {
+                    id: 50,
+                    sl: 25,
+                    tp: 0,
+                    offset: 0,
+                    symbol: 'AUDUSD',
+                    volume: 0.1
+                }
+            ]
+        },{   
+            id: 798,
+            accounts: [
+                {
+                    id: 19,
+                    sl: 25,
+                    tp: 0,
+                    offset: 0,
+                    symbol: 'NZDUSD',
+                    volume: 0.1
+                },
+                {
+                    id: 50,
+                    sl: 25,
+                    tp: 0,
+                    offset: 0,
+                    symbol: 'NZDUSD',
+                    volume: 0.1
+                }
+            ]
+        },
+
+
+// STRATEGY 2. HISTOGRAM ABOVE / BELOW TRESHOLD, CLOSE AT POSITIVE / NEGATIVE      
+        {   
+            id: 600,
+            accounts: [
+                {
+                    id: 20,
+                    sl: 2000,
+                    tp: 0,
+                    offset: 0,
+                    symbol: 'OIL.WTI',
+                    volume: 0.1
+                }
+            ]
+        }
+    ]
+}
+
