1221797a63c7c7ed85a595dd7a18f359fa73e8cd
Ray <raitis.cerkasovs@gmail.com>
2021-07-12 10:52:35 +0100

    rem sub st12

--- config/multiples.js
+++ config/multiples.js
@@ -1401,7 +1401,7 @@ module.exports = {
                 tp: 0,
                 offset: 0,
                 symbol: 'AUDUSD',
-                volume: 0.3
+                volume: 0.1
             }
         ]
     },
@@ -1414,7 +1414,7 @@ module.exports = {
                 tp: 0,
                 offset: 0,
                 symbol: 'NZDUSD',
-                volume: 0.3
+                volume: 0.1
             }
         ]
     },
@@ -1427,7 +1427,7 @@ module.exports = {
                 tp: 0,
                 offset: 0,
                 symbol: 'USDCHF',
-                volume: 0.3
+                volume: 0.1
             }
         ]
     },
@@ -1440,7 +1440,7 @@ module.exports = {
                 tp: 0,
                 offset: 0,
                 symbol: 'USDCAD',
-                volume: 0.3
+                volume: 0.1
             }
         ]
     },
@@ -1453,7 +1453,7 @@ module.exports = {
                 tp: 0,
                 offset: 0,
                 symbol: 'EURUSD',
-                volume: 0.3
+                volume: 0.1
             }
         ]
     },
@@ -1466,7 +1466,7 @@ module.exports = {
                 tp: 0,
                 offset: 0,
                 symbol: 'GBPUSD',
-                volume: 0.3
+                volume: 0.1
             }
         ]
     },
@@ -1479,7 +1479,7 @@ module.exports = {
                 tp: 0,
                 offset: 0,
                 symbol: 'USDJPY',
-                volume: 0.3
+                volume: 0.1
             }
         ]
     },
@@ -1492,7 +1492,7 @@ module.exports = {
                 tp: 0,
                 offset: 0,
                 symbol: 'EURCHF',
-                volume: 0.3
+                volume: 0.1
             }
         ]
     },
@@ -1505,7 +1505,7 @@ module.exports = {
                 tp: 0,
                 offset: 0,
                 symbol: 'EURGBP',
-                volume: 0.3
+                volume: 0.1
             }
         ]
     },
@@ -1518,7 +1518,7 @@ module.exports = {
                 tp: 0,
                 offset: 0,
                 symbol: 'EURJPY',
-                volume: 0.3
+                volume: 0.1
             }
         ]
     },
@@ -1531,7 +1531,7 @@ module.exports = {
                 tp: 0,
                 offset: 0,
                 symbol: 'GBPCHF',
-                volume: 0.3
+                volume: 0.1
             }
         ]
     },
@@ -1544,165 +1544,165 @@ module.exports = {
                 tp: 0,
                 offset: 0,
                 symbol: 'GBPJPY',
-                volume: 0.3
-            }
-        ]
-    },
-    // 12, SUB STRATEGY 11, 1H RSI 50 SELL AND BUY, 0.1
-    {   
-        id: 1200,
-        accounts: [
-            {
-                id: 12,
-                sl: 25,
-                tp: 0,
-                offset: 0,
-                symbol: 'AUDUSD',
-                volume: 0.1
-            }
-        ]
-    },
-    {   
-        id: 1201,
-        accounts: [
-            {                
-                id: 12,
-                sl: 25,
-                tp: 0,
-                offset: 0,
-                symbol: 'NZDUSD',
-                volume: 0.1
-            }
-        ]
-    },
-    {   
-        id: 1202,
-        accounts: [
-            {
-                id: 12,
-                sl: 25,
-                tp: 0,
-                offset: 0,
-                symbol: 'USDCHF',
-                volume: 0.1
-            }
-        ]
-    },
-    {   
-        id: 1203,
-        accounts: [
-            {
-                id: 12,
-                sl: 25,
-                tp: 0,
-                offset: 0,
-                symbol: 'USDCAD',
-                volume: 0.1
-            }
-        ]
-    },
-    {   
-        id: 1204,
-        accounts: [
-            {
-                id: 12,
-                sl: 25,
-                tp: 0,
-                offset: 0,
-                symbol: 'EURUSD',
-                volume: 0.1
-            }
-        ]
-    },
-    {   
-        id: 1205,
-        accounts: [
-            {
-                id: 12,
-                sl: 25,
-                tp: 0,
-                offset: 0,
-                symbol: 'GBPUSD',
-                volume: 0.1
-            }
-        ]
-    },
-    {   
-        id: 1206,
-        accounts: [
-            {
-                id: 12,
-                sl: 2500,
-                tp: 0,
-                offset: 0,
-                symbol: 'USDJPY',
-                volume: 0.1
-            }
-        ]
-    },
-    {   
-        id: 1207,
-        accounts: [
-            {
-                id: 12,
-                sl: 23,
-                tp: 0,
-                offset: 0,
-                symbol: 'EURCHF',
-                volume: 0.1
-            }
-        ]
-    },
-    {   
-        id: 1208,
-        accounts: [
-            {
-                id: 12,
-                sl: 18,
-                tp: 0,
-                offset: 0,
-                symbol: 'EURGBP',
-                volume: 0.1
-            }
-        ]
-    },
-    {   
-        id: 1209,
-        accounts: [
-            {
-                id: 12,
-                sl: 2500,
-                tp: 0,
-                offset: 0,
-                symbol: 'EURJPY',
-                volume: 0.1
-            }
-        ]
-    },
-    {   
-        id: 1210,
-        accounts: [
-            {
-                id: 12,
-                sl: 25,
-                tp: 0,
-                offset: 0,
-                symbol: 'GBPCHF',
-                volume: 0.1
-            }
-        ]
-    },
-    {   
-        id: 1211,
-        accounts: [
-            {
-                id: 12,
-                sl: 2500,
-                tp: 0,
-                offset: 0,
-                symbol: 'GBPJPY',
                 volume: 0.1
             }
         ]
     }
+    // // 12, SUB STRATEGY 11, 1H RSI 50 SELL AND BUY, 0.1
+    // {   
+    //     id: 1200,
+    //     accounts: [
+    //         {
+    //             id: 12,
+    //             sl: 25,
+    //             tp: 0,
+    //             offset: 0,
+    //             symbol: 'AUDUSD',
+    //             volume: 0.1
+    //         }
+    //     ]
+    // },
+    // {   
+    //     id: 1201,
+    //     accounts: [
+    //         {                
+    //             id: 12,
+    //             sl: 25,
+    //             tp: 0,
+    //             offset: 0,
+    //             symbol: 'NZDUSD',
+    //             volume: 0.1
+    //         }
+    //     ]
+    // },
+    // {   
+    //     id: 1202,
+    //     accounts: [
+    //         {
+    //             id: 12,
+    //             sl: 25,
+    //             tp: 0,
+    //             offset: 0,
+    //             symbol: 'USDCHF',
+    //             volume: 0.1
+    //         }
+    //     ]
+    // },
+    // {   
+    //     id: 1203,
+    //     accounts: [
+    //         {
+    //             id: 12,
+    //             sl: 25,
+    //             tp: 0,
+    //             offset: 0,
+    //             symbol: 'USDCAD',
+    //             volume: 0.1
+    //         }
+    //     ]
+    // },
+    // {   
+    //     id: 1204,
+    //     accounts: [
+    //         {
+    //             id: 12,
+    //             sl: 25,
+    //             tp: 0,
+    //             offset: 0,
+    //             symbol: 'EURUSD',
+    //             volume: 0.1
+    //         }
+    //     ]
+    // },
+    // {   
+    //     id: 1205,
+    //     accounts: [
+    //         {
+    //             id: 12,
+    //             sl: 25,
+    //             tp: 0,
+    //             offset: 0,
+    //             symbol: 'GBPUSD',
+    //             volume: 0.1
+    //         }
+    //     ]
+    // },
+    // {   
+    //     id: 1206,
+    //     accounts: [
+    //         {
+    //             id: 12,
+    //             sl: 2500,
+    //             tp: 0,
+    //             offset: 0,
+    //             symbol: 'USDJPY',
+    //             volume: 0.1
+    //         }
+    //     ]
+    // },
+    // {   
+    //     id: 1207,
+    //     accounts: [
+    //         {
+    //             id: 12,
+    //             sl: 23,
+    //             tp: 0,
+    //             offset: 0,
+    //             symbol: 'EURCHF',
+    //             volume: 0.1
+    //         }
+    //     ]
+    // },
+    // {   
+    //     id: 1208,
+    //     accounts: [
+    //         {
+    //             id: 12,
+    //             sl: 18,
+    //             tp: 0,
+    //             offset: 0,
+    //             symbol: 'EURGBP',
+    //             volume: 0.1
+    //         }
+    //     ]
+    // },
+    // {   
+    //     id: 1209,
+    //     accounts: [
+    //         {
+    //             id: 12,
+    //             sl: 2500,
+    //             tp: 0,
+    //             offset: 0,
+    //             symbol: 'EURJPY',
+    //             volume: 0.1
+    //         }
+    //     ]
+    // },
+    // {   
+    //     id: 1210,
+    //     accounts: [
+    //         {
+    //             id: 12,
+    //             sl: 25,
+    //             tp: 0,
+    //             offset: 0,
+    //             symbol: 'GBPCHF',
+    //             volume: 0.1
+    //         }
+    //     ]
+    // },
+    // {   
+    //     id: 1211,
+    //     accounts: [
+    //         {
+    //             id: 12,
+    //             sl: 2500,
+    //             tp: 0,
+    //             offset: 0,
+    //             symbol: 'GBPJPY',
+    //             volume: 0.1
+    //         }
+    //     ]
+    // }
 ]}
--- config/strategy12.js
+++ config/strategy12.js
@@ -1,93 +1,93 @@
-module.exports = {
-    id: 11,
-    name: "1H RSI 50 SELL / BUY",
-    disableParameterCheck: false,
-    parameterCount: 2,
-    subStrategies: [],
-    bind: [
-        {
-            symbol: "AUDUSD",
-            account: 1200
-        },
-        {
-            symbol: "NZDUSD",
-            account: 1201
-        },
-        {
-            symbol: "USDCHF",
-            account: 1202
-        },
-        {
-            symbol: "USDCAD",
-            account: 1203
-        },
-        {
-            symbol: "EURUSD",
-            account: 1204
-        },
-        {
-            symbol: "GBPUSD",
-            account: 1205
-        },
-        {
-            symbol: "USDJPY",
-            account: 1206
-        },
-        {
-            symbol: "EURCHF",
-            account: 1207
-        },
-        {
-            symbol: "EURGBP",
-            account: 1208
-        },
-        {
-            symbol: "EURJPY",
-            account: 1209
-        },
-        {
-            symbol: "GBPCHF",
-            account: 1210
-        },
-        {
-            symbol: "GBPJPY",
-            account: 1211
-        }
-    ],
-    // and
-    buy: [
-        {
-            key: "1Hlowerstart50",
-            value: 1
-        }
-    ],
-    // or
-    closeBuy: [
-        {
-            key: "1Hupperstart50",
-            value: 1
-        },
-        {
-            key: "1Hlowerstart50",
-            value: -1
-        }
-    ],
-    // and
-    sell: [
-        {
-            key: "1Hupperstart50",
-            value: 1
-        }
-    ],
-    // or
-    closeSell: [
-        {
-            key: "1Hupperstart50",
-            value: -1
-        },
-        {
-            key: "1Hlowerstart50",
-            value: 1
-        }
-    ]
-}
+// module.exports = {
+//     id: 11,
+//     name: "1H RSI 50 SELL / BUY",
+//     disableParameterCheck: false,
+//     parameterCount: 2,
+//     subStrategies: [],
+//     bind: [
+//         {
+//             symbol: "AUDUSD",
+//             account: 1200
+//         },
+//         {
+//             symbol: "NZDUSD",
+//             account: 1201
+//         },
+//         {
+//             symbol: "USDCHF",
+//             account: 1202
+//         },
+//         {
+//             symbol: "USDCAD",
+//             account: 1203
+//         },
+//         {
+//             symbol: "EURUSD",
+//             account: 1204
+//         },
+//         {
+//             symbol: "GBPUSD",
+//             account: 1205
+//         },
+//         {
+//             symbol: "USDJPY",
+//             account: 1206
+//         },
+//         {
+//             symbol: "EURCHF",
+//             account: 1207
+//         },
+//         {
+//             symbol: "EURGBP",
+//             account: 1208
+//         },
+//         {
+//             symbol: "EURJPY",
+//             account: 1209
+//         },
+//         {
+//             symbol: "GBPCHF",
+//             account: 1210
+//         },
+//         {
+//             symbol: "GBPJPY",
+//             account: 1211
+//         }
+//     ],
+//     // and
+//     buy: [
+//         {
+//             key: "1Hlowerstart50",
+//             value: 1
+//         }
+//     ],
+//     // or
+//     closeBuy: [
+//         {
+//             key: "1Hupperstart50",
+//             value: 1
+//         },
+//         {
+//             key: "1Hlowerstart50",
+//             value: -1
+//         }
+//     ],
+//     // and
+//     sell: [
+//         {
+//             key: "1Hupperstart50",
+//             value: 1
+//         }
+//     ],
+//     // or
+//     closeSell: [
+//         {
+//             key: "1Hupperstart50",
+//             value: -1
+//         },
+//         {
+//             key: "1Hlowerstart50",
+//             value: 1
+//         }
+//     ]
+// }
