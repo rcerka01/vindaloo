a44a312673904bbdfaf61464fdc95117b77137b8
Ray <raitis.cerkasovs@gmail.com>
2021-07-12 09:44:54 +0100

    disable st 12

--- config/strategy11.js
+++ config/strategy11.js
@@ -3,7 +3,7 @@ module.exports = {
     name: "1H RSI 50 SELL / BUY",
     disableParameterCheck: false,
     parameterCount: 2,
-    subStrategies: [12],
+    subStrategies: [],
     bind: [
         {
             symbol: "AUDUSD",
@@ -52,6 +52,54 @@ module.exports = {
         {
             symbol: "GBPJPY",
             account: 1111
+        },
+        {
+            symbol: "AUDUSD",
+            account: 1200
+        },
+        {
+            symbol: "NZDUSD",
+            account: 1201
+        },
+        {
+            symbol: "USDCHF",
+            account: 1202
+        },
+        {
+            symbol: "USDCAD",
+            account: 1203
+        },
+        {
+            symbol: "EURUSD",
+            account: 1204
+        },
+        {
+            symbol: "GBPUSD",
+            account: 1205
+        },
+        {
+            symbol: "USDJPY",
+            account: 1206
+        },
+        {
+            symbol: "EURCHF",
+            account: 1207
+        },
+        {
+            symbol: "EURGBP",
+            account: 1208
+        },
+        {
+            symbol: "EURJPY",
+            account: 1209
+        },
+        {
+            symbol: "GBPCHF",
+            account: 1210
+        },
+        {
+            symbol: "GBPJPY",
+            account: 1211
         }
     ],
     // and
